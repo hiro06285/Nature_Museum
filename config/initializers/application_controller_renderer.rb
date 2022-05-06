3696474c809777829c5d4b4dc504af54149920ad
hiro06285 <hiro.06285@gmail.com>
2022-05-06 08:40:30 +0000

    [Add]投稿一覧画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '20cd6413c896e1a6e9e5b5d71b82f1fd1b9d47e50c2f7dc0a3056e2d9e9034d7637db88e1b6c972dfdaf04d916caa7dee4644eb54314f7e85913625b49c80ded'
