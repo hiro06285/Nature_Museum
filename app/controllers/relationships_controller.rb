520107b8ee1c75d15e7cf83e7d66a7e5dcf64a90
hiro06285 <hiro.06285@gmail.com>
2022-05-30 07:27:07 +0000

    [Fix]通知機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,9 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
     @user.create_notification_follow!(current_user)
+    redirect_to request.referer
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
@@ -7,11 +9,12 @@ Rails.application.routes.draw do
     resources :post_image_comments, only: [:create, :destroy]
   end
   resources :genres, only: [:index, :create, :edit, :update]
-  resources :users do
+  resources :users, only: [:show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :notifications, only: [:index]
+  delete 'notifications/destroy_all' => 'notifications#destroy_all'
 
 end
