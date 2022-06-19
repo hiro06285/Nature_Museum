62155a4607e7ad7b3b3f9962318e8d37d594d9fd
hiro06285 <hiro.06285@gmail.com>
2022-06-19 08:14:13 +0000

    [Fix]エラー修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,17 +2,16 @@ module NotificationsHelper
   def notification_form(notification)
     @visiter = notification.visiter
     @comment = nil
-    your_item = link_to 'あなたの投稿', new_post_image_path(notification), style:"font-weight: bold;"
     @visiter_comment = notification.post_image_comment_id
     #notification.actionがfollowかlikeかcommentか
     case notification.action
       when "follow" then
-        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
+        tag.a(notification.visiter.name, href: user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
       when "like" then
-        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_item_path(notification.item_id), style:"font-weight: bold;")+"にいいねしました"
+        tag.a(notification.visiter.name, href: user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href: post_image_path(notification.post_image_id), style:"font-weight: bold;")+"にいいねしました"
       when "comment" then
         @comment = Comment.find_by(id: @visiter_comment)&.content
-        tag.a(@visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_item_path(notification.item_id), style:"font-weight: bold;")+"にコメントしました"
+        tag.a(@visiter.name, href: users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href: post_image_path(notification.post_image_comment_id), style:"font-weight: bold;")+"にコメントしました"
     end
   end
 end
