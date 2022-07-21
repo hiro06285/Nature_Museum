55a1dbcd26d7f2e0f2bc2d4bc0d9596b4e7c51d0
hiro06285 <hiro.06285@gmail.com>
2022-07-21 07:19:58 +0000

    [Fix]マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @post_image = PostImage.page(params[:page]).reverse_order
   end
 
   def edit
@@ -17,5 +18,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction, :address, :telephone_number, :email)
+  end
+
+  def post_image_params
+    params.require(:post_image).permit(:image)
   end
 end
